0eb626c6de55e250482b1cc2b37b8f6340295ed0
V1ADDD <rusakvlad2@gmail.com>
2025-04-11 12:34:57 +0300

    feat: mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  @ViewChild('nav', { static: false }) navRef!: ElementRef;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (this.isMenuOpen && this.navRef && !this.navRef.nativeElement.contains(event.target)) {
+      const burger = (event.target as HTMLElement).closest('.burger');
+      if (!burger) {
+        this.closeMenu();
+      }
+    }
+  }
 }
